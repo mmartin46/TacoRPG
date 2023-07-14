57807f46e04fd93ab31c7715ea9951b2ecefa746
Mitchell Martin <mmartin46@angelo.edu>
2023-07-13 21:37:51 -0500

    Refactoring

--- loader.cpp
+++ loader.cpp
@@ -108,11 +108,11 @@ void GameState::load()
    this->set_enemy_texture(SDL_CreateTextureFromSurface(this->get_renderer(), surface));
    SDL_FreeSurface(surface);
 
+   // Setting the constant textures.
 
-   tSetters.push_back(set_ground_texture);
-   
    //tSetters.push_back(set_boundBushLeft_Texture);
    //tSetters.push_back(set_boundBushRight_Texture);
+   tSetters.push_back(set_ground_texture);
    tSetters.push_back(set_boundBushDown_Texture);
    tSetters.push_back(set_boundBushUp_Texture);
    tSetters.push_back(set_boundBush_Texture);
@@ -127,12 +127,9 @@ void GameState::load()
    fileMap.insert({"sprites\\landscape\\boundary_bush.png", "load bb(): No texture"});
    fileMap.insert({"sprites\\landscape\\boundary_bush_up.png", "load bb_u(): No texture"});
    fileMap.insert({"sprites\\landscape\\boundary_bush_down.png", "load bb_d(): No texture"});
+   fileMap.insert({"sprites\\groundblock.png", "load groundblock(): No texture"});
    //fileMap.insert({"sprites\\landscape\\boundary_bush_right.png", "load bb_r(): No texture"});
    //fileMap.insert({"sprites\\landscape\\boundary_bush_left.png", "load bb_l(): No texture"});
-
-
-   fileMap.insert({"sprites\\groundblock.png", "load groundblock(): No texture"});
-
    setConstantSpriteTextures(surface);
 
 
@@ -159,7 +156,7 @@ void GameState::load()
    SDL_FreeSurface(surface);
 
 
-
+   // Animated Textures
 
    for (int i = 1; i <= 5; i++)
    {
