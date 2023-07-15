515bdb4af793a2ef583e65b880b2f49b6733b9f6
Martin <mitchell.martin@ymcadallas.org>
2023-07-15 11:58:02 -0500

    Setting Changes

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 
 int main(int argc, char **argv)
 {
+   // Intializes the game
    GameState game;
    SDL_Window *window = NULL;
    SDL_Renderer *renderer = NULL;
@@ -22,13 +23,23 @@ int main(int argc, char **argv)
 
    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
    game.set_renderer(renderer);
+
+   // Sets the screen size.
    SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, 0);
    SDL_RenderSetLogicalSize(renderer, SCREEN_WIDTH, SCREEN_HEIGHT);
+
    TTF_Init();
 
+   // Loads the textures for the game.
    game.load();
 
+
+   /*
+   If done is anything else than 0,
+   close the game.
+   */
    int done = 0;
+   // Game Loop
    while (!done)
    {
       done = game.events(window);
@@ -37,6 +48,7 @@ int main(int argc, char **argv)
       game.animate();
       game.render();
 
+      // Delay for CPU.
       std::this_thread::sleep_for(std::chrono::milliseconds(3));
       game.inc_time();
    }
